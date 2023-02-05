6428021a6ecb9159b863c917602aca7f0a05a1c7
TrevorTurkaly <123902025+TrevorTurkaly@users.noreply.github.com>
2023-02-05 22:12:47 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Assignment-3-c-programming/main.cpp
@@ -0,0 +1,23 @@
+/*
+I chose excercise 4 at the end of the chapter
+*/
+
+#include <iostream>
+
+int main() {
+//create 2 variables for the 2 numbers and rsult
+int number1, number2;
+int result;
+
+
+//assign values to the 2 variables number1 is 87 and number2 is 15
+number1 = 87;
+number2 = 15;
+result = number1 - number2;
+
+
+ // subtract number1 from number2 
+std::cout<<"The difference between the number 1 number 2 is = "<<result<<std::endl;
+
+  return 0;
+}
